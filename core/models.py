acf2bfadb7b63cda8f7b7b81333d98826ce6dbec
adhamtamer874-tech <adhamtamer874@gmail.com>
2026-05-11 20:26:53 +0300

    Add Job model with fields for job listings

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Job(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    company = models.CharField(max_length=255)
+    years = models.IntegerField()
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+    STATUS_CHOICES = [
+    ('open', 'Open'),    
+    ('closed', 'Closed'),
+    ]
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='open')
+    def __str__(self):
+        return self.title
